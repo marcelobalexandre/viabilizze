e016286938ed3b14bdbc13556e884a8c3f0bf658
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-04-06 20:19:48 -0300

    Rollback foundation-rails gem from version 5.5.1.1 to 5.5.1.0 to fix JavaScript bugs.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require foundation
+//= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function() { 
+  $(document).foundation(); 
+});
