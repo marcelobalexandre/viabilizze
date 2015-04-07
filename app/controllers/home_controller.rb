6fa4504f3c7aa660d4d2589be56ba490997290f0
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-04-06 22:21:13 -0300

    Fix sign out link error.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      
+      redirect_to projects_path
     else
       redirect_to new_user_session_path
     end
