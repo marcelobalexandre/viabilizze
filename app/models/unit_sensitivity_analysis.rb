76441e858bd95d8160df1c269c4cc22bec6c19fa
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-05-25 22:02:29 -0300

    Refactoring.

--- app/models/unit_sensitivity_analysis.rb
+++ app/models/unit_sensitivity_analysis.rb
@@ -6,77 +6,51 @@ class UnitSensitivityAnalysis < ActiveRecord::Base
   validates :sensitivity_analysis, presence: true
 
   def selling_price
-    selling_price = ((self.individualization_costs + self.construction_costs + self.land_acquisition_cost +
-                      self.exchanged_units_construction_costs + self.exchanged_units_expenses) *
-                     self.sensitivity_analysis.markup).round(2)
-    self.unit.exchanged ? 0.00 : selling_price
+    calculate_and_return_zero_if_exchanged do
+      costs_and_expenses = self.individualization_costs + self.construction_costs + self.land_acquisition_cost +
+                           self.exchanged_units_construction_costs + self.exchanged_units_expenses
+      (costs_and_expenses * self.sensitivity_analysis.markup).round(2)
+    end
   end
 
   def sales_commissions
-    sales_commissions = (self.sale_price * (self.sensitivity_analysis.sales_commissions_rate / 100)).round(2)
-    self.unit.exchanged ? 0.00 : sales_commissions
+    calculate_and_return_zero_if_exchanged { (self.sale_price * (self.sensitivity_analysis.sales_commissions_rate / 100)).round(2) }
   end
 
   def sales_taxes
-    sales_taxes = (self.sale_price * (self.sensitivity_analysis.sales_taxes_rate / 100)).round(2)
-    self.unit.exchanged ? 0.00 : sales_taxes
+    calculate_and_return_zero_if_exchanged { (self.sale_price * (self.sensitivity_analysis.sales_taxes_rate / 100)).round(2) }
   end
 
   def sales_charges
-    sales_charges = (self.sale_price * (self.sensitivity_analysis.sales_charges_rate / 100)).round(2)
-    self.unit.exchanged ? 0.00 : sales_charges
+    calculate_and_return_zero_if_exchanged { (self.sale_price * (self.sensitivity_analysis.sales_charges_rate / 100)).round(2) }
   end
 
   def individualization_costs
-    unless self.sensitivity_analysis.individualization_costs
-      return 0.00
+    calculate_and_return_zero_if_exchanged do
+      (self.sensitivity_analysis.individualization_costs / self.sensitivity_analysis.project.total_units_not_exchanged).round(2)
     end
-
-    individualization_costs = (self.sensitivity_analysis.individualization_costs /
-                               self.sensitivity_analysis.project.total_units_not_exchanged).round(2)
-    self.unit.exchanged ? 0.00 : individualization_costs
   end
 
   def land_acquisition_cost
-    unless self.sensitivity_analysis.land_acquisition_cost_per_total_area_not_exchanged
-      return 0.00
-    end
-
-    land_acquisition_cost = (self.sensitivity_analysis.land_acquisition_cost_per_total_area_not_exchanged * self.unit.total_area).round(2)
-    self.unit.exchanged ? 0.00 : land_acquisition_cost
+    calculate_and_return_zero_if_exchanged { (self.sensitivity_analysis.land_acquisition_cost_per_total_area_not_exchanged * self.unit.total_area).round(2) }
   end
 
   def construction_costs
-    unless self.sensitivity_analysis.cost_per_square_meter
-      return 0.00
-    end
-
     (self.sensitivity_analysis.cost_per_square_meter * self.unit.total_area).round(2)
   end
 
   def exchanged_units_construction_costs
-    unless self.sensitivity_analysis.exchanged_units_construction_costs_per_total_area_not_exchanged
-      return 0.00
+    calculate_and_return_zero_if_exchanged do
+      (self.unit.total_area * self.sensitivity_analysis.exchanged_units_construction_costs_per_total_area_not_exchanged).round(2)
     end
-
-    exchanged_units_construction_costs = (self.unit.total_area *
-                                          self.sensitivity_analysis.exchanged_units_construction_costs_per_total_area_not_exchanged).round(2)
-    self.unit.exchanged ? 0.00 : exchanged_units_construction_costs
   end
 
   def exchanged_units_expenses
-    unless self.sensitivity_analysis.exchanged_units_expenses_per_total_area_not_exchanged
-      return 0.00
-    end
-
-    exchanged_units_expenses = (self.unit.total_area * self.sensitivity_analysis.exchanged_units_expenses_per_total_area_not_exchanged).round(2)
-    self.unit.exchanged ? 0.00 : exchanged_units_expenses
+    calculate_and_return_zero_if_exchanged { (self.unit.total_area * self.sensitivity_analysis.exchanged_units_expenses_per_total_area_not_exchanged).round(2) }
   end
 
   def result
-    if self.unit.exchanged
-      0.00
-    else
+    calculate_and_return_zero_if_exchanged do
       (self.sale_price - self.individualization_costs - self.construction_costs -
        self.land_acquisition_cost - self.exchanged_units_construction_costs - self.exchanged_units_expenses -
        self.sales_commissions - self.sales_taxes - self.sales_charges).round(2)
@@ -84,10 +58,16 @@ class UnitSensitivityAnalysis < ActiveRecord::Base
   end
 
   def profit_rate
+    calculate_and_return_zero_if_exchanged { ((self.result / self.sale_price) * 100).round(2) }
+  end
+
+  private
+
+  def calculate_and_return_zero_if_exchanged(&block)
     if self.unit.exchanged
       0.00
     else
-      ((self.result / self.sale_price) * 100).round(2)
+      block.call
     end
   end
 end
